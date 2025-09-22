c925ed43ff4246f8a392e643777e1508d11bc3d2
siaheguelable <siaheguelable@gmail.com>
2025-09-22 22:38:27 +0200

    assigment two contact corrections made on  put and post

--- routes/user.js
+++ routes/user.js
@@ -1,93 +1,154 @@
 const routes = require('express').Router();
 const userController = require('../Controllers/User');
 
+// get all the user
 /**
  * @swagger
  * /users:
  *   get:
  *     summary: Get all users
+ *     tags: [users]
  *     responses:
  *       200:
- *         description: List of users
- *   post:
- *     summary: Create a new user
- *     parameters:
- *       - in: body
- *         name: user
- *         required: true
- *         schema:
- *           type: object
- *           properties:
- *             firstName:
- *               type: string
- *             lastName:
- *               type: string
- *             email:
- *               type: string
- *             favoriteColor:
- *               type: string
- *             birthday:
- *               type: string
- *               format: date
- *     responses:
- *       200:
- *         description: User created
- *
- * /users/{id}:
- *   get:
- *     summary: Get user by ID
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         type: string
- *     responses:
- *       200:
- *         description: User found
- *   put:
- *     summary: Update user by ID
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         type: string
- *       - in: body
- *         name: user
- *         required: true
- *         schema:
- *           type: object
- *           properties:
- *             firstName:
- *               type: string
- *             lastName:
- *               type: string
- *             email:
- *               type: string
- *             favoriteColor:
- *               type: string
- *             birthday:
- *               type: string
- *               format: date
- *     responses:
- *       200:
- *         description: User updated
- *   delete:
- *     summary: Delete user by ID
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         type: string
- *     responses:
- *       200:
- *         description: User deleted
+ *         description: List of products
  */
 
-// Routes
 routes.get('/', userController.getAllUsers);
+
+// post user
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a new users
+ *     tags: [users]
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: product
+ *         description: user to create
+ *         required: true
+ *         schema:
+ *           type: object
+ *           required:
+ *             - firtsname
+ *             - lastname
+ *             - email
+ *             - favoriteColor
+ *             - birthday
+ *
+ *           properties:
+ *             firstname:
+ *               type: string
+ *             lastname:
+ *               type: string
+ *             email:
+ *               type: string
+ *             favoriteColor:
+ *               type: string
+ *             birthday:
+ *               type: date
+ *
+ */
+
 routes.post('/', userController.createUser);
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     summary: Get a user by ID
+ *     tags: [users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Product found
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal server error
+ */
 routes.get('/:id', userController.getUserById);
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   put:
+ *     summary: Update a user by ID
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *                 example: John
+ *               lastName:
+ *                 type: string
+ *                 example: Doe
+ *               email:
+ *                 type: string
+ *                 example: john.doe@example.com
+ *               favoriteColor:
+ *                 type: string
+ *                 example: Blue
+ *               birthday:
+ *                 type: string
+ *                 format: date
+ *                 example: 1990-01-01
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *       400:
+ *         description: Validation failed
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Error updating user
+ */
+
 routes.put('/:id', userController.updateUser);
+
+// Delete a user by ID
+/**
+ * @swagger
+ * /users/{id}:
+ *   delete:
+ *     summary: Delete a user by ID
+ *     tags:
+ *       - Users
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user to delete
+ *     responses:
+ *       204:
+ *         description: User deleted successfully
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Error deleting user
+ */
+
 routes.delete('/:id', userController.deleteUser);
 
 // Optional: delete all users
